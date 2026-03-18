ff7ee14d723905c034ecd9996c876701fccf7d27
HalxDocs <kamsyejindu@gmail.com>
2026-03-19 00:15:30 +0100

    app done

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,37 @@
 package app
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/HalxDocs/lazydb/internal/db"
+	"github.com/HalxDocs/lazydb/internal/tui"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func Run(driver, dsn string) error {
+	// connect to database
+	database := db.New(db.Config{
+		Driver: driver,
+		DSN:    dsn,
+	})
+
+	if database == nil {
+		return fmt.Errorf("unsupported driver: %s (use postgres, mysql or sqlite)", driver)
+	}
+
+	if err := database.Connect(); err != nil {
+		return fmt.Errorf("could not connect: %w", err)
+	}
+	defer database.Close()
+
+	// start TUI
+	model := tui.NewModel(database)
+	p := tea.NewProgram(model, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	return nil
+}
